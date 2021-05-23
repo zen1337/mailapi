96d60675cc7c762cef3dce0dba589f25966bebe1
zen1337 <33397782+zen1337@users.noreply.github.com>
2021-05-23 13:02:32 +0200

    Add files via upload

---
+++ gMailApi_UI.py
@@ -0,0 +1,29 @@
+from kivy.app import App
+from kivy.uix.label import Label
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.button import Button
+from kivy.uix.textinput import TextInput
+
+class GridGrid(GridLayout):
+    def __init__(self, **kwargs):
+        # call grid layout constructor
+        super(GridGrid, self).__init__(**kwargs)
+        # set columns
+        self.cols = 2
+        # add widgets
+        self.add_widget(Label(text="Name: "))
+        # add Input Box
+        self.name = TextInput(text='test', multiline=True)
+        #self.wtf = Button(text='wtf test', size_hint_x=1, width=100)
+        self.add_widget(self.name)
+        self.add_widget(Button(text='wtf test', size_hint_x=1, width=100))
+
+class MainApp(App):
+    def build(self):
+        label = Label(text="testuje")
+        return GridGrid()
+        #label = Label(text="testuje", size_hint = (.5, .5), pos_hint = {'center_x': .5, 'center_y': .5})
+        #return label
+if __name__ == '__main__':
+    app = MainApp()
+    app.run()
